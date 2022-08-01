0b5eea3d8e65ecae0457856456ebd9558de2ccb2
Volodymyr <bugsbunny1163@gmail.com>
2022-08-01 14:00:41 +0300

    less 2 done

--- 002/002_Task2.sql
+++ 002/002_Task2.sql
@@ -35,3 +35,29 @@ Phone VARCHAR(15),
 Position VARCHAR(30),
 PRIMARY KEY(emp_id)
 );
+
+CREATE TABLE products(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(20),
+price INT,
+sup_id INT,
+FOREIGN KEY (sup_id) REFERENCES suppliers(sup_id)
+);
+
+CREATE TABLE orders(
+id INT AUTO_INCREMENT PRIMARY KEY,
+pur_id INT,
+emp_id INT,
+FOREIGN KEY(pur_id) REFERENCES purchasers(pur_id),
+FOREIGN KEY(emp_id) REFERENCES employees(emp_id)
+);
+
+CREATE TABLE order_details(
+order_id INT,
+prod_id INT,
+qty INT NOT NULL,
+PRIMARY KEY (order_id, prod_id),
+FOREIGN KEY(order_id) REFERENCES orders(id),
+FOREIGN KEY (prod_id) REFERENCES products(id)
+)
+
